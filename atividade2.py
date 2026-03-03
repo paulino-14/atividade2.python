2ea58e7fff41a6f6c74264afd77f456dfb25dd90
paulino-14 <joaopedrodesouzapaulino60@gmail.com>
2026-03-03 08:31:19 -0300

    Add files via upload

---
+++ atividade2.py
@@ -0,0 +1,9 @@
+
+num = int(input("Digite um número inteiro positivo: "))
+
+if num % 2 == 0:
+    resultado = num ** 2
+else:
+    resultado = num ** 3
+
+print("Resultado:", resultado)
---
+++ repetição.py
@@ -0,0 +1,9 @@
+cont = 1 
+while cont <= 10 : 
+    print ("Ola mundo!")
+    cont += 1  # cont = cont + 1
+    print ("FIM")
+    
+    print(list(range(5)))
+    print(list(range(1,10,2)))
+    print(list(range(1,10))) 
